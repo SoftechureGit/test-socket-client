2a999bc62af1f43f3e6bfde7eca1edd7c5f40cab
sagar-johari <joharisagar53@gmail.com>
2026-01-02 15:49:39 +0530

    changes with proper working

--- app/channel/[channel_id]/ChannelChat.tsx
+++ app/channel/[channel_id]/ChannelChat.tsx
@@ -571,7 +571,7 @@ function handleSaveEdit(
                       </span>
                     )}
 
-                    </div>
+                    </div> 
                   )
                   }
                  <div className="relative">
